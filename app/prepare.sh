5d549a16947d362983502bb24b2a6e419c9a5243
dodo920306 <dodo920306@gmail.com>
2024-06-09 03:34:42 +0000

    Remove image background

--- app/prepare.sh
+++ app/prepare.sh
@@ -4,6 +4,6 @@ python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py collectstatic --no-input
 mv frontend/cctv.png static/cctv.png
-mv frontend/light.jpg static/light.jpg
-mv frontend/sideWalk.png static/sideWalk.png
+mv frontend/lamp.png static/lamp.png
+mv frontend/sidewalk.png static/sidewalk.png
 echo "Preparation done!"
