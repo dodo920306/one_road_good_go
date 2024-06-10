9c110673951829c4f88614a2e73579ce25a05370
dodo920306 <dodo920306@gmail.com>
2024-06-10 08:33:20 +0000

    Finish sidewalk searching

--- app/app/views.py
+++ app/app/views.py
@@ -9,17 +9,32 @@ def data(request):
         s = request.GET["s"]
         w = request.GET["w"]
         e = request.GET["e"]
-        
-        response = {"applied": True, "error_msg": "", "cctv": list(CCTV.objects.filter(
-            Latitude__range=(s, n),
-            Longitude__range=(w, e)
-        ).values()), "lamp": list(LAMP.objects.filter(
-            Y__range=(s, n),
-            X__range=(w, e)
-        ).values()), "accident": list(ACCIDENT.objects.filter(
-            Y__range=(s, n),
-            X__range=(w, e)
-        ).values())}
+        isCCTV = request.GET["isCCTV"] == "true"
+        isLAMP = request.GET["isLAMP"] == "true"
+        isACCIDENT = request.GET["isACCIDENT"] == "true"
+        isSIDEWALK = request.GET["isSIDEWALK"] == "true"
+        response = {"applied": True, "error_msg": ""}
+        if (isCCTV):
+            response["cctv"] = list(CCTV.objects.filter(
+                Latitude__range=(s, n),
+                Longitude__range=(w, e)
+            ).values())
+        if (isLAMP):
+            response["lamp"] = list(LAMP.objects.filter(
+                Y__range=(s, n),
+                X__range=(w, e)
+            ).values())
+        if (isACCIDENT):
+            response["accident"] = list(ACCIDENT.objects.filter(
+                Y__range=(s, n),
+                X__range=(w, e)
+            ).values())
+        if (isSIDEWALK):
+            response["sidewalk"] = list(SIDEWALK.objects.all().values())
+            response["sidewalk_point"] = list(SIDEWALK_POINT.objects.filter(
+                Y__range=(s, n),
+                X__range=(w, e)
+            ).values())
     except Exception as e:
         response = {"applied": False, "error_msg": repr(e)}
     return JsonResponse(response)
